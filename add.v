3a5a3c8a91457db8a552c15c94c511711baa11c8
ryu <52441827+gajagajago@users.noreply.github.com>
2021-06-18 11:04:54 +0900

    Update add.v

--- add.v
+++ add.v
@@ -19,11 +19,11 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module add(
-	 input [7:0] in1,
+    input [7:0] in1,
     input [7:0] in2,
     output [7:0] out,
     input ctrl_aluop
 );
-    // only need add
+
     assign out = in1 + in2;
 endmodule
